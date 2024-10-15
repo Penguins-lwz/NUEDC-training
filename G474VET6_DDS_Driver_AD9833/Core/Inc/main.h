4b72131de615ff313e51aadf06c691bf513455ca
Penguins-lwz <hust.penguins@qq.com>
2024-10-15 11:59:04 +0800

    commit G474VET6_DDS_Driver_AD9833

---
+++ G474VET6_DDS_Driver_AD9833/Core/Inc/main.h
@@ -0,0 +1,93 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stdio.h"
+#include "string.h"
+#include "stdarg.h"
+#include "stdbool.h"
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LED_B_Pin GPIO_PIN_13
+#define LED_B_GPIO_Port GPIOC
+#define LED_G_Pin GPIO_PIN_14
+#define LED_G_GPIO_Port GPIOC
+#define LED_R_Pin GPIO_PIN_15
+#define LED_R_GPIO_Port GPIOC
+#define KEY1_Pin GPIO_PIN_0
+#define KEY1_GPIO_Port GPIOC
+#define KEY2_Pin GPIO_PIN_1
+#define KEY2_GPIO_Port GPIOC
+#define KEY3_Pin GPIO_PIN_2
+#define KEY3_GPIO_Port GPIOC
+#define KEY4_Pin GPIO_PIN_3
+#define KEY4_GPIO_Port GPIOC
+#define DDS_CS_Pin GPIO_PIN_6
+#define DDS_CS_GPIO_Port GPIOD
+#define DDS_SCL_Pin GPIO_PIN_3
+#define DDS_SCL_GPIO_Port GPIOB
+#define DDS_SDA_Pin GPIO_PIN_5
+#define DDS_SDA_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */

0ecd330aba5c3dbbecf4269da7699ab8345b7bd1
Penguins-lwz <hust.penguins@qq.com>
2025-04-02 13:32:08 +0800

    update depository structure

--- project_ti/02_led/MDK-ARM/syscfg_penguins.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set SDK_JSON="C:\ti\mspm0_sdk_2_03_00_07\.metadata\product.json"
-set SYSCFG_BAT="C:\ti\sysconfig_1.22.0\sysconfig_cli.bat"
-set SYSCFG_DIR="%~dp0..\MSPM0-SDK"
-set SYSCFG_FILE="%~dp0..\MSPM0-SDK\project.syscfg"
-
-call %SYSCFG_BAT% -o %SYSCFG_DIR% -s %SDK_JSON% --compiler keil %SYSCFG_FILE%
--- project_ti/12_pga_non_inverting/Core/Src/opa.c
+++
@@ -1,9 +0,0 @@
-#include "opa.h"
-
-void myOPA_Config(OA_Regs *hopa, DL_OPA_GAIN newGain)
-{
-	DL_OPA_disable(hopa);
-	DL_OPA_setGain(hopa, newGain);
-	DL_OPA_setInvertingInputChannel(hopa, newGain == DL_OPA_GAIN_N0_P1 ? DL_OPA_NSEL_RTOP : DL_OPA_NSEL_RTAP);
-	DL_OPA_enable(hopa);
-}
